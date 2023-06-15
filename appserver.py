c21d656f6881a6b11a5f37f2e3dce52750300580
bohnerjosh <squash0002@gmail.com>
2023-06-14 19:05:44 -0500

    initial commit

---
+++ appserver.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify, abort
+import hashlib
+from flask_sqlalchemy import SQLAlchemy
+import os 
+import re
+
+email_regex = re.compile(r"^(.+)@(.+)\.(.{3})")
+app = Flask(__name__)
+app.secret_key = b'ABCDEFG#(*JXKNCW:Q"vwads6it7y'
+
+'''
+sqlite_uri = 'sqlite:///' + os.path.abspath(os.path.curdir) + '/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = sqlite_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import User
+'''
+
+@app.route('/')
+def index():
+    return redirect(url_for("home"))
+
+@app.route('/home/', methods=['GET'])
+def home():
+    return render_template("home.html")
+
+@app.route('/services/', methods=['GET'])
+def services():
+    return render_template("services.html")
+
+@app.route('/contact/', methods=['GET'])
+def contact():
+    return render_template("contact.html")
+
+@app.route('/contact/', methods=["POST"])
+def post_contact():
+    message = ""
+
+    email = request.form["email"]
+    name = request.form["name"]
+    inquiry = request.form["message"]
+
+    if not re.fullmatch(email_regex, email):
+        message = "Invalid email address"
+    elif name == "":
+        message = "Name cannot be blank"
+    elif inquiry == "":
+        message = "Message cannot be blank"
+
+    if message == "":
+        return render_template("sent.html")
+    else:
+        return render_template("contact.html", message=message)
+
+@app.route('/sent/', methods=['GET'])
+def sent():
+    return render_template("sent.html")
+
+@app.route('/store/', methods=['GET'])
+def store():
+    return render_template("store.html")
